696ae5422f648b1034485612c99b86d596ac9d2d
Panxoty <panxitto40@gmail.com>
2024-12-09 22:50:05 -0300

    dark-mode

--- js/app.js
+++ js/app.js
@@ -4,3 +4,14 @@ const mobileMenu = document.getElementById('mobile-menu');
 botonMenu.addEventListener('click', () => {
     mobileMenu.classList.toggle('hidden');
 })
+
+//Modo oscuro
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const htmlElement = document.documentElement;
+const darkModeIcon = document.getElementById('dark-mode-icon');
+
+// Alternar clase 'dark' en el elemento raíz
+darkModeToggle.addEventListener('click', () => {
+    const isDarkMode = htmlElement.classList.toggle('dark');
+    darkModeIcon.textContent = isDarkMode ? '☀️' : '🌙'; // Cambiar ícono
+});
